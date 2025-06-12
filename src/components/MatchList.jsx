3a36c42c10e23b9a440e35a9ab217e2c8c475a62
Alfio Puglisi <puglisi_algio@ciao.it>
2025-06-12 08:47:16 +0200

    Possibilità di eliminare partite singolarmente

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -13,6 +13,12 @@ export default function MatchList() {
     setMatches([]);
   };
 
+  const handleRemoveMatch = (indexMatch) => {
+    const updateMatches = matches.filter((_, index) => index !== indexMatch);
+    setMatches(updateMatches);
+    localStorage.setItem("matches", JSON.stringify(updateMatches))
+  }
+
   return (
     <div>
       <h2>Partite salvate</h2>
@@ -25,6 +31,7 @@ export default function MatchList() {
               <span>{match.giocatore1}</span> vs <span>{match.giocatore2}</span>
               <br />
               Punteggio: {match.punteggio} - Data: {match.data}
+              <button onClick={()=>handleRemoveMatch(index)}>Elimina Partita</button>
             </li>
           ))}
         </ul>
