3dccfb3bc7794e6e58a7baf1171c7af2b7190bef
Alfio Puglisi <puglisi_algio@ciao.it>
2025-06-12 09:43:05 +0200

    Aggiunto stile all'header: centratura e miglioramento font

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import MatchList from "./components/MatchList";
 import MatchForm from "./components/MatchForm";
+import MatchList from "./components/MatchList";
 
 function App() {
   return (
-    <div>
-      <h1>Tennis Match Tracker 🎾</h1>
-      <p>Benvenuto! Qui registrerai le tue partite.</p>
-      <MatchList/>
-      <MatchForm/>
-    </div>
+    <>
+      <header>
+          <h1>Tennis Match Tracker 🎾</h1>
+          <p>Benvenuto! Qui registrerai le tue partite.</p>
+      </header>
+      <main>
+        <MatchForm />
+        <MatchList />
+      </main>
+    </>
   );
 }
 
