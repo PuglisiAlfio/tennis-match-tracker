ae8cddb5203b2045b8d80df2643619a995e297a2
Alfio Puglisi <puglisi_algio@ciao.it>
2025-07-02 09:24:39 +0200

    Isola il reducer del form in un file separato per maggiore leggibilità e riutilizzo in caso di necessità

--- src/components/MatchForm.jsx
+++ src/components/MatchForm.jsx
@@ -1,29 +1,7 @@
 import { useReducer } from "react";
+import { initialState, formReducer} from "../reducers/matchReducer.js"
 
 import styleForm from "../styles/matchForm.module.css"
-
-// Stato iniziale del form
-const initialState = {
-  giocatore1: "",
-  giocatore2: "",
-  punteggio: "",
-  data: "",
-  error: "",
-};
-
-// Funzione reducer per gestire gli aggiornamenti di stato
-function formReducer(state, action) {
-  switch (action.type) {
-    case "SET_FIELD":
-      return { ...state, [action.field]: action.value };
-    case "SET_ERROR":
-      return { ...state, error: action.error };
-    case "RESET":
-      return initialState;
-    default:
-      return state;
-  }
-}
 
 export default function MatchForm({ matches, onSetMatches }) {
   const [state, dispatch] = useReducer(formReducer, initialState);
---
+++ src/reducers/matchReducer.js
@@ -0,0 +1,22 @@
+// Stato iniziale del form
+const initialState = {
+  giocatore1: "",
+  giocatore2: "",
+  punteggio: "",
+  data: "",
+  error: "",
+};
+
+// Funzione reducer per gestire gli aggiornamenti di stato
+export function formReducer(state, action) {
+  switch (action.type) {
+    case "SET_FIELD":
+      return { ...state, [action.field]: action.value };
+    case "SET_ERROR":
+      return { ...state, error: action.error };
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+}
